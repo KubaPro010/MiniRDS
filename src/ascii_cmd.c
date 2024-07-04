9f5f899ebdce3508aaf8638ecf760c421d978a0b
KubaPro010 <kuba.pro010@gmail.com>
2024-07-04 14:17:38 +0200

    im dumb

--- src/ascii_cmd.c
+++ src/ascii_cmd.c
@@ -260,25 +260,24 @@ void process_ascii_cmd(unsigned char *str) {
 			uint8_t arg_count;
 			rds_af_t new_af;
 			float af[MAX_AFS], *af_iter;
-			printf("AF\n");
 
 			arg_count = sscanf((char *)arg,
-				"%f,%f,%f,%f,%f " /* AF list */
-				"%f,%f,%f,%f,%f "
-				"%f,%f,%f,%f,%f "
-				"%f,%f,%f,%f,%f "
+				"%f,%f,%f,%f,%f," /* AF list */
+				"%f,%f,%f,%f,%f,"
+				"%f,%f,%f,%f,%f,"
+				"%f,%f,%f,%f,%f,"
 				"%f,%f,%f,%f,%f",
 			&af[0],  &af[1],  &af[2],  &af[3],  &af[4],
 			&af[5],  &af[6],  &af[7],  &af[8],  &af[9],
 			&af[10], &af[11], &af[12], &af[13], &af[14],
 			&af[15], &af[16], &af[17], &af[18], &af[19],
 			&af[20], &af[21], &af[22], &af[23], &af[24]);
-			printf("%d\n", arg_count);
 			af_iter = af;
 			memset(&new_af, 0, sizeof(struct rds_af_t));
 			while ((arg_count-- - 1) != 0) {
 				add_rds_af(&new_af, *af_iter++);
 			}
+			set_rds_af(new_af);
 			return;
 		}
 	}
